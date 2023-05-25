b1e373c2296302b759b8f0923fb34116d3dbf244
khalidwasif590 <khalidwasif590@gmail.com>
2023-05-25 10:29:25 +0600

    practice file

---
+++ practice.js
@@ -0,0 +1,11 @@
+let name = "I am from Bangladesh";
+let food = ["Cake", "Chocolate", "Ice-Cream"];
+let person = {
+    name: "A A M Khalid Hasan",
+    position: "Software Engineer",
+    age : 25
+
+}
+for(var x in name){
+    console.log(`index ${x} and item ${name[x]}`)
+}
